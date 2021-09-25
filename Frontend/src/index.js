f22670fb4dc3b45a29b3021767c0976f3bf4c4f7
jeuub <jeuub.bodur@gmail.com>
2021-09-25 14:13:39 +0300

    Добавляет фронтенд

---
+++ Frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import App from "./components/app/app";
+
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector(`#root`)
+);
+
+
