5d05c0a04c8e8455fbd06656322c927efc46cba7
Matvey Bouhartsev <programmer@bouhartsev.top>
2021-09-25 19:03:37 +0300

    Add auth and reg pages

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -3,6 +3,8 @@ import { Switch, Route, Router } from "react-router-dom";
 import browserHistory from "../../browser-history";
 import MainPage from "../../pages/main-page/main-page";
 import Header from "./header/header";
+import RegistrationPage from "../../pages/registration-page/registration-page";
+import AuthPage from "../../pages/auth-page/auth-page";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" render={() => <MainPage />} />
+        <Route exact path="/registration" render={() => <RegistrationPage />} />
+        <Route exact path="/auth" render={() => <AuthPage />} />
       </Switch>
     </Router>
   );
