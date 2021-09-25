98fe37f4e28c06580dec64d28788a22e6b2eaf27
Eiyub Bodur <71319621+jeuub@users.noreply.github.com>
2021-09-25 22:36:01 +0300

    Revert "Change structure"

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import React, { Fragment } from "react";
 
 const Footer = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg" style={{ height: 50 }}>
-      <Container>
-        <Navbar.Brand style={{ fontSize: 20 }}>@Мой Город</Navbar.Brand>
-      </Container>
-    </Navbar>
-  );
+  return(
+    <Fragment>
+      <footer className="footer text-center" style={{width: '100%', backgroundColor: '#282C34', color: 'white'}}>
+        © 2021,  Мой Город - moigorod.ru
+      </footer>
+    </Fragment>
+  )
 };
+
 export default Footer;
